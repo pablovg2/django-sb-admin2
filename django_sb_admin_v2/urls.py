314a67a7b8e8d7d4c55e4184ef6253ef38a4fe30
Lorenzo Landolfi <sirlandalf@live.it>
2014-07-26 23:06:43 +0200

    added admin page and others demo pages

--- django_sb_admin_v2/urls.py
+++ django_sb_admin_v2/urls.py
@@ -7,6 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_sb_admin_v2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^(\w+)/$', 'sb_admin2.views.home'),
+    url(r'^$', 'sb_admin2.views.home'),
 
-    url(r'^$', 'sb_admin2.views.home')
+
 )
--- sb_admin2/views.py
+++ sb_admin2/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
-def home(request):
-	return render(request,"index.html")
+from sb_admin2.models import PanelTabsTest
+def home(request,page=""):
+	paneltest = PanelTabsTest.objects.all()
+	return render(request,(page if page else "index") + ".html",{"tabs":paneltest})
