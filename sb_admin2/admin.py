cb728745afa99dfef08820701531f53b4ca43305
Lorenzo Landolfi <sirlandalf@live.it>
2014-07-26 23:08:13 +0200

    models for testing tabs and pills panels

---
+++ sb_admin2/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from sb_admin2 import models
+
+admin.site.register(models.PanelTabsTest)
--- sb_admin2/models.py
+++ sb_admin2/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class PanelTabsTest(models.Model):
+	tabname = models.CharField(max_length=30)
+	tabtitle = models.CharField(max_length=100)
+	content = models.TextField()
+	def __str__(self):
+		return "Name: '"+self.tabname+"' ,Title: '"+self.tabtitle+"'"
